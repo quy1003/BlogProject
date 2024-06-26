ec9ec9392310f73d9c74508e24334a9ea0b524ab
quy1003 <quy021002@gmail.com>
2024-06-26 17:49:38 +0700

    push project

--- src/index.js
+++ src/index.js
@@ -5,14 +5,21 @@ const hbs  = require('express-handlebars');
 const app = express()
 const port = 3000
 
+//Ghi nhật kí yêu cầu HTTP
 app.use(morgan('combined'))
+
+//Tìm và phục vụ tài nguyên tĩnh
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.engine('hbs', hbs.engine({extname: '.hbs'}));
 app.set('view engine', 'hbs')
-app.set('views', path.join(__dirname, 'resources', 'views'))
-// console.log('dir: '+path.join(__dirname, 'resources', 'views'))
 
+//Định nghĩa cho biết nơi cần tìm các tập tin template
+app.set('views', path.join(__dirname, 'resources', 'views'))
+
+//Trả về endpoints
 app.get('/', (req,res) => res.render('home'))
 app.get('/news', (req,res) => res.render('news'))
+
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
